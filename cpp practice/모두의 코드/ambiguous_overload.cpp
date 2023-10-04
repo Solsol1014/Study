9647ba19d07b2890e490b196a5c37f4b304c7dda
Solsol1014 <k202211360@konkuk.ac.kr>
2023-10-05 00:09:00 +0900

    231005_0008

---
+++ cpp practice/모두의 코드/ambiguous_overload.cpp
@@ -0,0 +1,22 @@
+//모호한 오버로딩
+#include <iostream>
+
+void print(int x) {
+    std::cout << "int : " << x << std::endl;
+}
+
+void print(char x) {
+    std::cout << "double : " << x << std::endl;
+}
+
+int main() {
+    int a = 1;
+    char b = 'c';
+    double c = 3.2f;
+
+    print(a);
+    print(b);
+    //print(c);
+
+    return 0;
+}
