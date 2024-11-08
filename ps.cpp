9157432d2eb420e7ee32b08fbcf256cd94410c9b
Solsol1014 <k202211360@konkuk.ac.kr>
2024-11-08 16:46:41 +0900

    20241108-1646

--- ps.cpp
+++ ps.cpp
@@ -1,42 +1,11 @@
 #include <iostream>
-#include <cmath>
+//#include <cstdio>
 
 int main() {
-    int min;
-    int max;
-    std::cin >> min;
-    std::cin >> max;
-
-    float minsqrt, maxsqrt;
-    minsqrt = std::sqrt(min);
-    maxsqrt = std::sqrt(max);
-
-    int sum = 0;
-    if (minsqrt-(int)minsqrt == 0) {
-        int intminsqrt = (int)minsqrt;
-
-        while(intminsqrt <= maxsqrt) {
-            sum += intminsqrt*intminsqrt;
-            intminsqrt++;
-        }
-        std::cout << sum << '\n';
-        std::cout << (int)minsqrt*minsqrt << '\n';
-    }
-    else {
-        int intminsqrt = (int)minsqrt + 1;
-
-        if (intminsqrt > maxsqrt) {
-            std::cout << -1 << '\n';
-        }
-        else {
-            while(intminsqrt <= maxsqrt) {
-                sum += intminsqrt*intminsqrt;
-                intminsqrt++;
-            }
-            std::cout << sum << '\n';
-            std::cout << ((int)minsqrt+1)*((int)minsqrt+1) << '\n';
-        }
-    }
+    int a, b;
+    
+    std::cin >> a >> b;
+    std::cout << a+b;
 
     return 0;
 }
