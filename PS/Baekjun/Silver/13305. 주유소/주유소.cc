2705c39c44755e0d951f6a5ddadbb886c491a64c
장서현 <106345468+Solsol1014@users.noreply.github.com>
2024-11-13 23:23:36 +0900

    [Silver III] Title: 주유소, Time: 48 ms, Memory: 3568 KB, Score: 58 point -BaekjoonHub

---
+++ PS/Baekjun/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+//#include <cstdio>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> citylen;
+    for (int i = 0;i<n-1;i++) {
+        int tmp;
+        std::cin >> tmp;
+        citylen.push_back(tmp); 
+    }
+    std::vector<int> price;
+    for (int i = 0;i<n;i++) {
+        int tmp;
+        std::cin >> tmp;
+        price.push_back(tmp);
+    }
+
+    std::vector<int>::iterator begin = price.begin();
+    int sum = 0;
+    std::vector<int>::iterator iter;
+
+    while (true) {
+        for (iter = begin;iter!=price.end();iter++) {
+            if (*begin > *iter) {
+                for (int i = std::distance(price.begin(), begin);i<std::distance(price.begin(), iter);i++) {
+                    sum += *begin * citylen[i];
+                }
+
+                begin = iter;
+                break;
+            }
+        }
+
+        if (iter == price.end()) {
+            if(begin == iter-1) {
+                break;
+            }
+            else {
+                for (int i = std::distance(price.begin(), begin);i<price.size();i++) {
+                    sum += *begin * citylen[i];
+                }
+                break;
+            }
+        }
+    }
+
+    std::cout << sum;
+    return 0;
+}
