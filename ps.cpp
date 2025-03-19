d8cda617d570838b2bafb7158b0317dfa8f10afd
Solsol1014 <k202211360@konkuk.ac.kr>
2025-03-19 18:41:48 +0900

    250319-1841

--- ps.cpp
+++ ps.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 //#include <cstdio>
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
-    
-    int n, m;
-    std::cin >> n;
-    std::cin >> m;
 
-    std::vector<int> numbers;
-    for (int i = 0;i<n;i++) {
+    int sum = 0;
+    std::vector<int> nines;
+    for (int i = 0;i<9;i++) {
         int tmp;
         std::cin >> tmp;
-        numbers.push_back(tmp); 
+        sum += tmp;
+        nines.push_back(tmp); 
     }
-    
-    int max = 0;
 
-    for (std::vector<int>::iterator itr = numbers.begin() ; itr != numbers.end()-2 ; itr++) {
-        for (std::vector<int>::iterator itr2 = itr+1 ; itr2 != numbers.end()-1 ; itr2++) {
-            for (std::vector<int>::iterator itr3 = itr2+1 ; itr3 != numbers.end() ; itr3++) {
-                int tmp = *itr + *itr2 + *itr3;
-                if(tmp <= m && tmp > max) {
-                    max = tmp;
+    std::vector<int> sevens;
+    bool trigger = false;
+    for (std::vector<int>::iterator itr = nines.begin() ; itr < nines.end()-1 ; itr++) {
+        for (std::vector<int>::iterator itr2 = itr+1 ; itr2 < nines.end() ; itr2++) {
+            if (sum-*itr-*itr2 == 100) {
+                for (std::vector<int>::iterator itr3 = nines.begin() ; itr3 < nines.end() ; itr3++) {
+                    if(itr3!=itr) {
+                        if(itr3!=itr2) {
+                            sevens.push_back(*itr3);
+                        }
+                    }
                 }
+                trigger = true;
+                break;
             }
         }
+        if(trigger)
+            break;
     }
 
-    std::cout << max;
+    std::sort(sevens.begin(), sevens.end());
+    for (std::vector<int>::iterator itr = sevens.begin() ; itr != sevens.end() ; itr++) {
+        std::cout << *itr << "\n";
+    }
+
     return 0;
 }
