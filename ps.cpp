af80b73515ee85852ba38382ab41b3c52cecbbf2
Solsol <021014sarah@gmail.com>
2025-03-24 16:48:15 +0900

    250324-1648

--- ps.cpp
+++ ps.cpp
@@ -4,41 +4,50 @@
 //#include <cstdio>
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
 
-    int sum = 0;
-    std::vector<int> nines;
-    for (int i = 0;i<9;i++) {
+    int n;
+    std::cin >> n;
+    if (n==1 || n==2) {
+        std::cout << n;
+        return 0;
+    }
+
+    std::vector<int> fruits1, fruits2;
+    for (int i=0 ; i<n/2 ; i++) {
         int tmp;
         std::cin >> tmp;
-        sum += tmp;
-        nines.push_back(tmp); 
+        fruits1.push_back(tmp);
+    }
+    for (int i=n/2 ; i<n ; i++) {
+        int tmp;
+        std::cin >> tmp;
+        fruits2.push_back(tmp);
     }
 
-    std::vector<int> sevens;
-    bool trigger = false;
-    for (std::vector<int>::iterator itr = nines.begin() ; itr < nines.end()-1 ; itr++) {
-        for (std::vector<int>::iterator itr2 = itr+1 ; itr2 < nines.end() ; itr2++) {
-            if (sum-*itr-*itr2 == 100) {
-                for (std::vector<int>::iterator itr3 = nines.begin() ; itr3 < nines.end() ; itr3++) {
-                    if(itr3!=itr) {
-                        if(itr3!=itr2) {
-                            sevens.push_back(*itr3);
-                        }
-                    }
-                }
-                trigger = true;
-                break;
-            }
-        }
-        if(trigger)
-            break;
+    // for(std::vector<int>::iterator itr = fruits1.begin() ; itr != fruits1.end() ; itr++) {
+    //     std::cout << *itr << "\n";
+    // }
+
+    // std::cout << "\n";
+
+    // for(std::vector<int>::iterator itr = fruits2.begin() ; itr != fruits2.end() ; itr++) {
+    //     std::cout << *itr << "\n";
+    // }
+
+    int type2[2];
+    
+    if(n%2==1) {
+        type2[0] = fruits2[0];
+        fruits2.erase(fruits2.begin());
     }
 
-    std::sort(sevens.begin(), sevens.end());
-    for (std::vector<int>::iterator itr = sevens.begin() ; itr != sevens.end() ; itr++) {
-        std::cout << *itr << "\n";
+    std::vector<int>::reverse_iterator itr1 = fruits1.rbegin();
+    std::vector<int>::iterator itr2 = fruits2.begin();
+
+    for (int i = 0; i<n/2 ; i++) {
+        
     }
 
     return 0;
