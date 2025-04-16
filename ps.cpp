0ec0d39aeb24b3a3a3d5a33af4a433072acb18ca
Solsol1014 <k202211360@konkuk.ac.kr>
2025-04-16 11:14:52 +0900

    250416-1114

--- ps.cpp
+++ ps.cpp
@@ -9,46 +9,96 @@
 // #include <iterator>
 // #include <map>
 
-int min(int n, int arrsave[]) {
-    if(n==1 || n==2 || n==3) {
-        arrsave[n] = n;
-        return n;
-    }
-    else {
-        if(arrsave[n-2]==0) {
-            min(n-2, arrsave);
-        }
-        if (arrsave[n-1]==0) {
-            min(n-1, arrsave);
-        }
-        
+int answers[1000001] = {0, };
 
-        int min1 = arrsave[n-1]+1;
-        int min2 = arrsave[n-2]+2;
-
-        if(min1<min2) {
-            arrsave[n] = min1;
-            return min1;
-        }
-        else {
-            arrsave[n] = min2;
-            return min2;
-        }
-    }
-}
+void calculate(int n);
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
+    answers[2] = 1;
+    answers[3] = 1;
+
     int n;
     std::cin >> n;
-    int arrsave[n+1];
-    for(int i = 0 ; i<=n ; i++) {
-        arrsave[i] = 0;
+
+    if(n!=1) {
+        calculate(n);
+        std::cout << answers[n];
     }
-    
-    std::cout << min(n, arrsave);
+    if(n==1) {
+        std::cout << 0;
+    }
 
     return 0;
+}
+
+void calculate(int n) {
+    if(answers[n]!=0) {
+        return;
+    }
+    else {
+        bool onlytwo = false, onlythree = false, twothree = false;
+        if(n%2==0) {
+            if(n%3==0) {
+                twothree = true;
+            }
+            else {
+                onlytwo = true;
+            }
+        }
+        else if(n%3==0) {
+            onlythree = true;
+        }
+
+        int min = 0;
+        if(twothree) {
+            if(answers[n/3]==0 && n/3!=1)
+                calculate(n/3);
+            if(answers[n/2]==0 && n/2!=1)
+                calculate(n/2);
+            if(answers[n-1]==0)
+                calculate(n-1);
+            
+            if(answers[n/3] < answers[n/2])
+                min = answers[n/3];
+            else
+                min = answers[n/2];
+            
+            if(min > answers[n-1])
+                min = answers[n-1];
+        }
+        else if(onlythree) {
+            if(answers[n/3]==0 && n/3!=1)
+                calculate(n/3);
+            if(answers[n-1]==0)
+                calculate(n-1);
+
+            if(answers[n/3] < answers[n-1])
+                min = answers[n/3];
+            else
+                min = answers[n-1];
+        }
+        else if(onlytwo) {
+            if(answers[n/2]==0 && n/2!=1)
+                calculate(n/2);
+            if(answers[n-1]==0)
+                calculate(n-1);
+
+            if(answers[n/2] < answers[n-1])
+                min = answers[n/2];
+            else
+                min = answers[n-1];
+        }
+        else {
+            if(answers[n-1]==0)
+                calculate(n-1);
+            
+            min = answers[n-1];
+        }
+    
+        answers[n] = min + 1;
+        return;
+    }
 }
